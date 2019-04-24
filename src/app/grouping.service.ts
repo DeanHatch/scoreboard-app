46c65baf6421330db9467945b5e530ecc5481201
Dean Hatch <dean.hatch@milehighsoftware.com>
2019-04-23 21:14:37 -0400

    Added primitive caching to Groupings Service

--- src/app/grouping.service.ts
+++ src/app/grouping.service.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from, of, Observable } from 'rxjs';
 
 import { Competition } from './models/competition';
 import { Grouping } from './models/grouping';
 
 @Injectable()
 export class GroupingService {
-  private groupings: Grouping[] =
+  private currentCompetition: Competition;
+  private groupings: Grouping[];
+  private groupingz: Grouping[] =
 	[ { _id: 1, name: 'DEF Grouping', variety: 'Grouping Variety',
  	  scheduleable: true, standingsable: true, bracketable: true, parent_id: null,
           subgroupings: [], teams: []}
 	];
 
   constructor(
-	private http: HttpClient
-  ) { }
+	  private http: HttpClient
+	  ) { }
 
   urlFrom(arr) {
     arr.unshift("http://localhost:3000");
@@ -22,8 +25,16 @@ export class GroupingService {
   }
   
   getGroupingsFor(comp) {
-    return this.http.get<Grouping[]>(this.urlFrom(["competitions",comp.id,"groupings"]),
-	  { responseType: "json" } );
+    if (this.currentCompetition == comp) {
+    console.log('We Already Have It: ',this.currentCompetition);
+    return of(this.groupings); 
+    } else {
+    this.currentCompetition = comp;
+    console.log('New Current Competition: ',this.currentCompetition);
+    this.http.get<Grouping[]>(this.urlFrom(["competitions",comp.id,"groupings"]),
+	  { responseType: "json" } ).subscribe((grpngs: Grouping[]) => {this.groupings = grpngs; console.log('X: ',this.groupings)});
+    return of(this.groupings); 
+	} // end of else...
   }
 
   getGroupingFor(comp, grp_id) {
